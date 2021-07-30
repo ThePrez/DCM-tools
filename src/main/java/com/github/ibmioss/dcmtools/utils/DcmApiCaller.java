7698422c2074dd5056742d715bba871f95c3b874
Jesse Gorzinski <jgorzinski@gmail.com>
2021-07-29 20:48:47 -0500

    convert newlines and add new API call

--- src/main/java/com/github/ibmioss/dcmtools/utils/DcmApiCaller.java
+++ src/main/java/com/github/ibmioss/dcmtools/utils/DcmApiCaller.java
@@ -37,6 +37,32 @@ public class DcmApiCaller implements Closeable {
         }
     }
 
+    public void callQycdUpdateCertUsage(String _appId, String _certStoreName, String _certId) throws PropertyVetoException, AS400SecurityException, ErrorCompletingRequestException, IOException, InterruptedException, ObjectDoesNotExistException {
+        final ProgramCall program = new ProgramCall(m_conn);
+        final String programName = "/QSYS.LIB/QYKMIMPK.PGM";
+        final ProgramParameter[] parameterList = new ProgramParameter[8];
+        // 1     Application ID  Output  Char(*)
+        parameterList[0] = new ProgramParameter(new AS400Text(_appId.length()).toBytes(_appId));
+        // 2 Length of application ID Input Binary(4)
+        parameterList[1] = new ProgramParameter(new AS400Bin4().toBytes(_appId.length()));
+        // 3 Certificate store name Input Char(*)
+        parameterList[2] = new ProgramParameter(new AS400Text(_certStoreName.length()).toBytes(_certStoreName));
+        // 4 Length of certificate store name Input Binary(4)
+        parameterList[3] = new ProgramParameter(new AS400Bin4().toBytes(_certStoreName.length()));
+        // 5 Certificate ID type Input Char(*)
+        parameterList[4] = new ProgramParameter(new AS400Text(1).toBytes("1"));
+        // 6 Certificate ID Input Char(*)
+        parameterList[5] = new ProgramParameter(new AS400Text(_certId.length()).toBytes(_certId));
+        // 7 Length of certificate ID Input Binary(4)
+        parameterList[6] = new ProgramParameter(new AS400Bin4().toBytes(_certId.length()));
+        // 8   Error code  I/O     Char(*)
+        final ErrorCodeParameter ec = new ErrorCodeParameter(true, true);
+        parameterList[7] = ec;
+
+        program.setProgram(programName, parameterList);
+        // Run the program.
+        runProgram(program, ec);
+    }
     // QykmImportKeyStore
     public void callQykmImportKeyStore(final String _dcmStore, final String _dcmStorePw, final String _dcmImportFile, final String _importFilePw) throws PropertyVetoException, AS400SecurityException, ErrorCompletingRequestException, IOException, InterruptedException, ObjectDoesNotExistException {
         final ProgramCall program = new ProgramCall(m_conn);
@@ -76,15 +102,19 @@ public class DcmApiCaller implements Closeable {
 
         program.setProgram(programName, parameterList);
         // Run the program.
-        if (!program.run()) {
-            for (final AS400Message msg : program.getMessageList()) {
+        runProgram(program, ec);
+    }
+
+    private void runProgram(ProgramCall _program, ErrorCodeParameter _ec) throws AS400SecurityException, ErrorCompletingRequestException, IOException, InterruptedException, ObjectDoesNotExistException {
+        if (!_program.run()) {
+            for (final AS400Message msg : _program.getMessageList()) {
                 // Show each message.
                 System.err.println(StringUtils.colorizeForTerminal("" + msg, TerminalColor.BRIGHT_RED));
             }
             throw new IOException("DCM API Call failure");
         }
-        final String errorMessageId = ec.getMessageID();
-        for (final AS400Message msg : program.getMessageList()) {
+        final String errorMessageId = _ec.getMessageID();
+        for (final AS400Message msg : _program.getMessageList()) {
             // Show each message.
             System.out.println(StringUtils.colorizeForTerminal("" + msg, TerminalColor.CYAN));
         }
